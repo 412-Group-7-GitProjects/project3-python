5e84f2dd41a9c62281d3ae468d5aba3778b3e947
jjag23 <112784347+jjag23@users.noreply.github.com>
2022-09-19 14:03:32 -0500

    Update linereadv2.py

--- linereadv2.py
+++ linereadv2.py
@@ -1,3 +1,3 @@
-with open(r"E:\demos\files\read_demo.txt", 'r') as fp:
+with open(r"webURL", 'r') as fp:
     num_lines = sum(1 for line in fp if line.rstrip())
     print('Total lines:', num_lines) 
