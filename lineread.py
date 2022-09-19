c52935ab2fa5c0c20cf6196f0d772f5af774feda
jjag23 <112784347+jjag23@users.noreply.github.com>
2022-09-19 10:16:12 -0500

    Update lineread.py

--- lineread.py
+++ lineread.py
@@ -1,7 +1,4 @@
-Python 3.7.4 (v3.7.4:e09359112e, Jul  8 2019, 14:54:52) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
->>> #code to read line of log
+#code to read line of log
 #
 def _count_generator(reader):
     b = reader(1024 * 1024)
